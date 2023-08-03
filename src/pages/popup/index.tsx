d315f3e68adc3d89a4eb9b90dd13b2bfb7e51d1f
ben jie ming <39157935+benjieming421@users.noreply.github.com>
2023-08-03 16:50:28 +0800

    Initial commit

---
+++ src/pages/popup/index.tsx
@@ -0,0 +1,11 @@
+import styles from './style.less';
+
+const Popup = () => {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.title}>popup page</h1>
+    </div>
+  );
+};
+
+export default Popup;
